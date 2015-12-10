acaeab19052794f43a82c00fc8278c39c490c279
rdmclin2 <rdmclin2@163.com>
2015-12-11 00:51:06 +0800

    debug

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('wechat',wechat);
+app.use('/wechat',wechat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
